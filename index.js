855f08413638081390b40da8da65108d0e8849dc
xinxin8816 <8501725+xinxin8816@users.noreply.github.com>
2022-07-16 15:18:39 +0800

    fix local model

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const multer = require('multer')
-const jpeg = require('jpeg-js')
 
 const tf = require('@tensorflow/tfjs-node')
 const nsfw = require('nsfwjs')
@@ -9,21 +8,6 @@ const app = express()
 const upload = multer()
 
 let _model
-
-const convert = async (img) => {
-  // Decoded image in UInt8 Byte array
-  const image = await jpeg.decode(img, true)
-
-  const numChannels = 3
-  const numPixels = image.width * image.height
-  const values = new Int32Array(numPixels * numChannels)
-
-  for (let i = 0; i < numPixels; i++)
-    for (let c = 0; c < numChannels; ++c)
-      values[i * numChannels + c] = image.data[i * 4 + c]
-
-  return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
-}
 
 app.post('/api', upload.single('image'), async (req, res) => {
   if (!req.file) res.status(400).send('Missing image multipart/form-data')
@@ -40,7 +24,7 @@ app.get('/', async (req, res) => {
 })
 
 const load_model = async () => {
-    _model = await nsfw.load()
+    _model = await nsfw.load('https://nsfwjs.com/quant_mid/')
 }
 
 load_model().then(() => app.listen(process.env.PORT || 5000))
