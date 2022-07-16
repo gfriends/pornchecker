ab112510270d37029d3a905b1826f3fab5d7630a
xinxin8816 <8501725+xinxin8816@users.noreply.github.com>
2022-07-16 15:32:08 +0800

    fix local model

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', async (req, res) => {
 })
 
 const load_model = async () => {
-    _model = await nsfw.load('https://nsfwjs.com/quant_mid/',{ type: 'graph' })
+    _model = await nsfw.load('file://./model/',{type: 'graph'})
 }
 
 load_model().then(() => app.listen(process.env.PORT || 5000))
